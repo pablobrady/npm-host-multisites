4592793891235a728992282bbeba08a27e1fd2d5
Paul Brady <pbrady@Har.local>
2015-07-05 20:13:13 -0700

    Added a simple TestSite1

---
+++ testsite1/app_testsite1.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.get('/', function (req, res) {
+  // res.send('Hello World?');
+  fs.readFile('./public/index.html', function (err, data) {
+      res.send(data.toString());
+  });
+});
+
+var server = app.listen(9001, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+
