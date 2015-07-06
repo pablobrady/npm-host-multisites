d2c8dd392cd3c9988689af6fbe455d0cc0422b55
Paul Brady <pbrady@Har.local>
2015-07-05 20:23:12 -0700

    Added simple site files for testsite2

---
+++ testsite2/app_testsite2.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  // res.send('Hello World?');
+  fs.readFile('./public/index.html', function (err, data) {
+      res.send(data.toString());
+  });
+});
+
+
+
+var server = app.listen(9002, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+
